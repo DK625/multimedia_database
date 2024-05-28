dcde845584e82155f9ca584890f966e9f21dba30
DK625 <minhha10c8@gmail.com>
2024-05-28 14:09:37 +0700

    tmp

--- data_process_preparing/data_preparing.py
+++ data_process_preparing/data_preparing.py
@@ -1,30 +1,47 @@
 import cv2
 import os
+from rembg import remove
+from PIL import Image
 
 
-def preprocess_image(image_path, target_size=(500, 500)):
-    # only resize
+def delete_background(image):
+    input_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    output_pil = remove(input_pil)
+    output = cv2.cvtColor(np.array(output_pil), cv2.COLOR_RGB2BGR)
+    return output
+
+
+def resize_image(image_path, target_size=(500, 500)):
     image = cv2.imread(image_path)
+    if image is None:
+        raise FileNotFoundError(f"Không thể đọc file ảnh từ đường dẫn: {image_path}")
     resized_image = cv2.resize(image, target_size)
     return resized_image
+
+
+
+def preprocess_image(image_path):
+    image_resized = resize_image(image_path)
+    image_deleted_background = remove(image_resized)
+    # # only resize
+    # image = cv2.imread(image_path)
+    # resized_image = cv2.resize(image, target_size)
+    return image_deleted_background
 
 
 def preprocess_images():
     if not os.path.exists(preprocessed_images):
         os.makedirs(preprocessed_images)
 
-    for sub_folder in os.listdir(raw_data_folder):
-        sub_folder_path = os.path.join(raw_data_folder, sub_folder)
-        if not os.path.exists(os.path.join(preprocessed_images, sub_folder)):
-            os.makedirs(os.path.join(preprocessed_images, sub_folder))
-        if os.path.isdir(sub_folder_path):
-            for filename in os.listdir(sub_folder_path):
-                image_path = os.path.join(sub_folder_path, filename)
-                preprocessed_image = preprocess_image(image_path)
-                cv2.imwrite(os.path.join(preprocessed_images, sub_folder, filename), preprocessed_image)
+    for filename in os.listdir(raw_data_folder):
+        image_path = os.path.join(raw_data_folder, filename)
+        preprocessed_image = preprocess_image(image_path)
+        cv2.imwrite(os.path.join(preprocessed_images, filename), preprocessed_image)
 
 
 ROOT_PATH = os.path.dirname(__file__)
 raw_data_folder = os.path.join(ROOT_PATH, 'raw_data')
-preprocessed_images = os.path.join(ROOT_PATH, 'preprocessed_images')
+preprocessed_images = os.path.join(ROOT_PATH, 'delete_background_images')
 preprocess_images()
+
+# python3 data_process_preparing/data_preparing.py
---
+++ data_process_preparing/same_statio.py
@@ -0,0 +1,41 @@
+import cv2
+import os
+import numpy as np
+from data_preparing import preprocessed_images, ROOT_PATH
+
+input_folder = preprocessed_images
+# output_folder = 'D:\\workspace\\multimedia_database\\data_process_preparing\\same_statio\\desert_mountains'
+output_folder = os.path.join(ROOT_PATH, 'same_statio')
+target_size = (300, 300)  # Kích thước mong muốn của vật thể
+
+# Lặp qua tất cả các tệp tin trong thư mục đầu vào
+for filename in os.listdir(input_folder):
+    if filename.endswith(('.jpg', '.jpeg', '.png')):  # Chỉ xử lý các tệp tin hình ảnh
+        image_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
+
+        # Đọc hình ảnh
+        img = cv2.imread(image_path)
+
+        # Chỉnh sửa kích thước vật thể
+        resized_object = cv2.resize(img, target_size)
+
+        # Khởi tạo khung hình mới có kích thước 500x500
+        frame = np.zeros((500, 500, 3), dtype=np.uint8)
+
+        # Tính toán tọa độ để đặt vật thể vào giữa khung hình
+        x = int((500 - target_size[0]) / 2)
+        y = int((500 - target_size[1]) / 2)
+
+        # Tính toán tọa độ của vật thể trong khung hình
+        obj_x = x
+        obj_y = y
+        obj_width = target_size[0]
+        obj_height = target_size[1]
+
+        # Đặt vật thể vào khung hình
+        frame[obj_y:obj_y+obj_height, obj_x:obj_x+obj_width] = resized_object
+
+        # Lưu hình ảnh đã chỉnh sửa vào thư mục đầu ra
+        cv2.imwrite(output_path, frame)
+# python3 data_process_preparing/same_statio.py
